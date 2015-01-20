792565112c846d1a2076b91e014901ccb0485c57
Rolando Perez <me@rolilink.com>
2015-01-20 12:19:15 -0500

    adding error handling to properties/create and traslating errors to spanish

--- public/js/app/views/property-form.js
+++ public/js/app/views/property-form.js
@@ -1,4 +1,18 @@
 define(['jquery','backbone','underscore','backbone.validation','jquery.serializeObject'],function($,Backbone,_){
+	
+	var error_translate = {
+		"Capacity is required":"Por favor llenar el campo de capacidad.",
+		"Price is required":"Por favor llenar el campo de precio.",
+		"Title is required":"Por favor llenar el campo de titulo.",
+		"Description is required":"Por favor llenar el campo de descripcion.",
+		"Address is required":"Por favor llenar el campo de direccion.",
+		"Title must be between 20 and 120 characters":"El campo titulo debe tener entre 20 y 120 caracteres.",
+		"Description must be between 40 and 500 characters":"El campo descripcion debe tener entre 40 y 500 caracteres.",
+		"Address must be between 10 and 70 characters":"La direccion debe tener entre 10 y 70 caracteres.",
+		"Price must be greater than or equal to 1":"El precio debe ser una cantidad valida mayor a 0.",
+		"Capacity must be greater than or equal to 1":"La capacidad debe ser una cantidad valida mayor a 0."
+	};
+
 	return Backbone.View.extend({
 		events:{
 			'click #btn-create':'validate'
@@ -12,6 +26,8 @@ define(['jquery','backbone','underscore','backbone.validation','jquery.serialize
 			this.model.set(data);
 			this.model.amenitiesToArray();
 			this.model.locToArray();
+			this.$el.find(".alert").hide();
+			this.$el.find(".alert ul").empty();
 			if(this.model.isValid(true)){
 				this.submit();
 			}
@@ -25,11 +41,13 @@ define(['jquery','backbone','underscore','backbone.validation','jquery.serialize
 		onValid: function(v,attr){
 			var input = v.$el.find('[name="' + attr + '"]');
 			$(input).parent().removeClass('has-error').addClass('has-success');
-			$(input).parent().find('.alert').remove();
 		},
 		onInvalid: function(v,attr,error){
+			console.log(error);
 			var input = v.$el.find('[name="' + attr + '"]');
 			$(input).parent().removeClass('has-success').addClass('has-error').append('<div class="alert alert-danger>' + error + '</div>');
+			v.$el.find(".alert ul").append("<li data-attr='"+ attr + "'>" +  error_translate[error] + "</li>");
+			v.$el.find(".alert").show();
 		},
 		submit:function(){
 			this.model.once('sync',this.onApiResponse,this);
@@ -41,7 +59,8 @@ define(['jquery','backbone','underscore','backbone.validation','jquery.serialize
 			this.trigger('property_created',{id:response.property._id})
 		},
 		onErrorResponse: function(response){
-			console.log('error:',response.error);
+			this.$el.find(".alert ul").append("<li> Ha Sucedido un error en el servidor intenta mas tarde.</li>");
+			this.$el.find(".alert").show();
 		}
 	});
 });
