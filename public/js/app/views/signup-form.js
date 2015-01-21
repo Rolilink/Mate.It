cd11b211068789beab8d65b5ac4781b708539261
Rolando Perez <me@rolilink.com>
2015-01-21 01:14:39 -0500

    resolving some errors

--- public/js/app/views/signup-form.js
+++ public/js/app/views/signup-form.js
@@ -6,10 +6,10 @@ define(['jquery','backbone','underscore','backbone.validation','jquery.serialize
 		"Password is required":"Por favor llenar el campo de contraseña.",
 		"Name must be between 2 and 100 characters":"El campo nombre debe contener entre 2 y 100 caracteres.",
 		"Username must be between 6 and 20 characters":"El nombre de usuario debe tener entre 6 y 20 caracteres.",
-		"Email must be a valid email","Introduzca un correo",
+		"Email must be a valid email":"Introduzca un correo",
 		"Password must be at least 6 characters":"La contraseña almenos debe tener 6 caracteres."
 	};
-	
+
 	return Backbone.View.extend({
 		events:{
 			'click #btn-signup':'validate'
