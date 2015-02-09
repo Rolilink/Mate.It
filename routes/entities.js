8d9d54c074115751c98aad43fed9909de1c347f7
Rolando Perez <me@rolilink.com>
2015-02-09 11:15:27 -0500

    starting api refactoring

--- routes/entities.js
+++
@@ -1,6 +0,0 @@
-app.get('/api/entities/countries',function(req,res,next){
-	seneca.act({controller:'entities',entity:'country',action:'list'},function(err,result){
-		if(err) return res.status(500);
-		return res.status(200).json(result.countries);
-	});
-})
