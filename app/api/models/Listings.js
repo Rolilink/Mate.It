5db2f96fb8a984877de78391b2680c2b57da3607
Rolando Perez <me@rolilink.com>
2015-02-09 23:09:20 -0500

    added attributes to listing model / added in,unique, required validations / test first then implement

--- app/api/models/Listings.js
+++ app/api/models/Listings.js
@@ -8,7 +8,69 @@
 module.exports = {
 
   attributes: {
-
+    capacity: {
+      type: 'integer',
+      required: true
+    },
+    listed: {
+      type: 'boolean',
+      defaultsTo: true
+    },
+    address: 'string',
+    price: {
+      type: 'float',
+      required: true
+    },
+    roomType: {
+      type: 'string',
+      defaultsTo: 'private',
+      in: [
+        'private',
+        'shared'
+      ]
+    },
+    propertyType: {
+      type: 'string',
+      defaultsTo: 'apartment',
+      in: [
+        'apartment',
+        'house'
+      ]
+    },
+    description: {
+      type: 'string',
+      required: true
+    },
+    title: {
+      type: 'string',
+      required: true
+    },
+    genderAllowed: {
+      type: 'string',
+      defaultsTo: 'both',
+      in: [
+        'both',
+        'male',
+        'female'
+      ]
+    },
+    amenities: {
+      type: 'array',
+      in: [
+        'kitchen',
+        'internet',
+        'tv',
+        'wifi',
+        'air_conditioning',
+        'washer',
+        'dryer',
+        'cable_tv',
+        'pets_allowed',
+        'gym',
+        'pool',
+        'smoking_allowed'
+      ]
+    }
   }
 };
 
